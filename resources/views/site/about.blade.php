c6e473c052e6c977e6adade14e5040cacc9b7b5f
PNMGROUP\catur <catur.crh@gmail.com>
2023-09-30 23:19:42 +0800

    Update menu pengembang

--- resources/views/site/about.blade.php
+++ resources/views/site/about.blade.php
@@ -70,14 +70,14 @@
     <div class="container-xxl py-5">
         <div class="container">
             <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
-                <h6 class="section-title bg-white text-center text-primary px-3">Instructors</h6>
-                <h1 class="mb-5">Expert Instructors</h1>
+                <h6 class="section-title bg-white text-center text-primary px-3">Tim</h6>
+                <h1 class="mb-5">Pengembang</h1>
             </div>
             <div class="row g-4">
                 <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
                     <div class="team-item bg-light">
                         <div class="overflow-hidden">
-                            <img class="img-fluid" src={{"site/img/team-1.jpg"}} alt="">
+                            <img src={!! url('assets/images/users/Pengembang1.png') !!} alt="" height="350" width="100%">
                         </div>
                         <div class="position-relative d-flex justify-content-center" style="margin-top: -23px;">
                             <div class="bg-light d-flex justify-content-center pt-2 px-1">
@@ -87,15 +87,15 @@
                             </div>
                         </div>
                         <div class="text-center p-4">
-                            <h5 class="mb-0">Instructor Name</h5>
-                            <small>Designation</small>
+                            <h5 class="mb-0">Muh. Alim Marhadi, S.Pd.,M.Pd</h5>
+                            <small>Dosen UHO</small>
                         </div>
                     </div>
                 </div>
                 <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
                     <div class="team-item bg-light">
                         <div class="overflow-hidden">
-                            <img class="img-fluid" src={{"site/img/team-2.jpg"}} alt="">
+                            <img src={!! url('assets/images/users/Pengembang2.png') !!} alt="" height="350">
                         </div>
                         <div class="position-relative d-flex justify-content-center" style="margin-top: -23px;">
                             <div class="bg-light d-flex justify-content-center pt-2 px-1">
@@ -105,15 +105,15 @@
                             </div>
                         </div>
                         <div class="text-center p-4">
-                            <h5 class="mb-0">Instructor Name</h5>
-                            <small>Designation</small>
+                            <h5 class="mb-0">Maysara., S.Pd.,M.Pd</h5>
+                            <small>Dosen UHO</small>
                         </div>
                     </div>
                 </div>
                 <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
                     <div class="team-item bg-light">
                         <div class="overflow-hidden">
-                            <img class="img-fluid" src={{"site/img/team-3.jpg"}} alt="">
+                            <img  src={!! url('assets/images/users/Pengembang3.png') !!} alt="" height="350" width="100%">
                         </div>
                         <div class="position-relative d-flex justify-content-center" style="margin-top: -23px;">
                             <div class="bg-light d-flex justify-content-center pt-2 px-1">
@@ -123,15 +123,15 @@
                             </div>
                         </div>
                         <div class="text-center p-4">
-                            <h5 class="mb-0">Instructor Name</h5>
-                            <small>Designation</small>
+                            <h5 class="mb-0">Fatahu, S.Pd., M Si</h5>
+                            <small>Dosen UHO</small>
                         </div>
                     </div>
                 </div>
                 <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.7s">
                     <div class="team-item bg-light">
                         <div class="overflow-hidden">
-                            <img class="img-fluid" src={{"site/img/team-4.jpg"}} alt="">
+                            <img  src={!! url('assets/images/users/Pengembang4.png') !!} alt="" height="350" width="100%">
                         </div>
                         <div class="position-relative d-flex justify-content-center" style="margin-top: -23px;">
                             <div class="bg-light d-flex justify-content-center pt-2 px-1">
@@ -141,8 +141,8 @@
                             </div>
                         </div>
                         <div class="text-center p-4">
-                            <h5 class="mb-0">Instructor Name</h5>
-                            <small>Designation</small>
+                            <h5 class="mb-0">Sulastri</h5>
+                            <small>Mahasiswa UHO</small>
                         </div>
                     </div>
                 </div>
